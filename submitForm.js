22345eb2b3136fd6393b95c766f42ac4e9459bfa
Jhans830 <Jhans830@gmail.com>
2017-05-07 15:27:15 -0600

    Update and rename example.js to submitForm.js

--- submitForm.js
+++ submitForm.js
@@ -5,12 +5,11 @@ var port = require('system').env.PORT || 8080; // default back to 8080
 // start a server on port 8080 and register a request listener
 server.listen(port, function(request, response) {
 
- console.log('Loading a web page');
 var page = require('webpage').create();
-var url = 'http://pages.s4.exacttarget.com/page.aspx?QS=472529ec60bdf32ace076b30a52580087daefea3fc019925f12fc53d64ba4a21';
-page.open(url, function (status) {
-    //Page is loaded!
-    page.render('phantomjs.png');
-    phantom.exit();
-  });
+
+page.open('URL WITH FORM', function() {
+        page.evaluate(function() {
+            document.forms[0].submit();
+        });
 });
+  });
